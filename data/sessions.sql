eee7ecdaf33c9f883424ed8edbb361de4c1c1163
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2019-12-13 13:35:30 -0500

    Create sessions.sql

---
+++ data/sessions.sql
@@ -0,0 +1,25 @@
+
+-- Database presets
+PRAGMA encoding = "UTF-8";	-- Default encoding set to UTF-8
+PRAGMA auto_vacuum = "2";	-- File size improvement
+PRAGMA temp_store = "2";	-- Memory temp storage for performance
+PRAGMA journal_mode = "WAL";	-- Performance improvement
+PRAGMA secure_delete = "1";	-- Privacy improvement
+
+-- Visitor/User sessions
+CREATE TABLE sessions(
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	session_id TEXT DEFAULT NULL COLLATE NOCASE,
+	session_data TEXT DEFAULT NULL COLLATE NOCASE,
+	created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);-- --
+CREATE UNIQUE INDEX idx_session_id ON sessions( session_id );-- --
+CREATE INDEX idx_session_created ON sessions( created DESC );-- --
+CREATE INDEX idx_session_updated ON sessions( updated DESC );-- --
+
+CREATE TRIGGER session_update AFTER UPDATE ON sessions
+BEGIN
+	UPDATE sessions SET updated = CURRENT_TIMESTAMP 
+		WHERE id = NEW.id;
+END;
