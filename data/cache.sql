dccf08ab003da4ab0cd6d7e4944ab4c10c3a3b20
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2019-12-13 13:36:09 -0500

    Create cache.sql

---
+++ data/cache.sql
@@ -0,0 +1,59 @@
+
+-- Database presets
+PRAGMA encoding = "UTF-8";	-- Default encoding set to UTF-8
+PRAGMA auto_vacuum = "2";	-- File size improvement
+PRAGMA temp_store = "2";	-- Memory temp storage for performance
+PRAGMA journal_mode = "WAL";	-- Performance improvement
+PRAGMA secure_delete = "1";	-- Privacy improvement
+
+-- Cache tables
+CREATE TABLE caches (
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+	cache_id TEXT NOT NULL, 
+	ttl INTEGER NOT NULL, 
+	content TEXT NOT NULL, 
+	expires DATETIME DEFAULT NULL,
+	created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+	updated DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);-- --
+
+CREATE UNIQUE INDEX idx_caches_on_cache_id ON caches ( cache_id ASC );-- --
+CREATE INDEX idx_caches_on_expires ON caches ( expires DESC );-- --
+CREATE INDEX idx_caches_on_created ON caches ( created ASC );-- --
+CREATE INDEX idx_caches_on_updated ON caches ( updated );-- --
+
+-- Cache triggers
+CREATE TRIGGER cache_after_insert AFTER INSERT ON caches FOR EACH ROW 
+BEGIN
+	-- Generate expiration
+	UPDATE caches SET 
+		expires = datetime( 
+			( strftime( '%s','now' ) + NEW.ttl ), 
+			'unixepoch' 
+		) WHERE rowid = NEW.rowid;
+	
+	-- Clear expired data
+	DELETE FROM caches WHERE 
+		strftime( '%s', expires ) < 
+		strftime( '%s', updated );
+END;-- --
+
+-- Change only update period when TTL is empty
+CREATE TRIGGER cache_after_update AFTER UPDATE ON caches FOR EACH ROW 
+WHEN NEW.updated < OLD.updated AND NEW.ttl = 0
+BEGIN
+	UPDATE caches SET updated = CURRENT_TIMESTAMP 
+		WHERE rowid = NEW.rowid;
+END;-- --
+
+-- Change expiration period when TTL exists
+CREATE TRIGGER cache_after_update_ttl AFTER UPDATE ON caches FOR EACH ROW 
+WHEN NEW.updated < OLD.updated AND NEW.ttl <> 0
+BEGIN
+	-- Change expiration
+	UPDATE caches SET updated = CURRENT_TIMESTAMP, 
+		expires = datetime( 
+			( strftime( '%s','now' ) + NEW.ttl ), 
+			'unixepoch' 
+		) WHERE rowid = NEW.rowid;
+END;
