0a5f0a8a9e6b05884ef516c615ff048031691abc
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2021-03-16 11:56:39 -0400

    Create Message.php

---
+++ lib/src/Message.php
@@ -0,0 +1,81 @@
+<?php declare( strict_types = 1 );
+/**
+ *  @file	/lib/src/Message.php
+ *  @brief	Common server/client message type
+ */
+namespace PubCabin;
+
+class Message {
+	
+	/**
+	 *  Content MIME type
+	 *  @var string
+	 */
+	public $content_type;
+	
+	/**
+	 *  Message headers list
+	 *  @var array
+	 */
+	public $headers;
+	
+	/**
+	 *  Message headers list
+	 *  @var array
+	 */
+	public $lv_headers;
+	
+	/**
+	 *  Message source or destination URI
+	 *  @var string
+	 */
+	protected $uri;
+	
+	/**
+	 *  Message protocol. E.G. HTTP 1.1
+	 *  @var string
+	 */
+	protected $protocol;
+	
+	/**
+	 *  Current message querystring or path attachment
+	 *  @var string
+	 */
+	protected $querystring;
+	
+	/**
+	 *  Core settings and configuration 
+	 *  @var PubCabin\Config
+	 */
+	protected $config;
+	
+	/**
+	 *  Main cabin
+	 *  @var PubCabin\Cabin
+	 */
+	protected $cabin;
+	
+	public function __construct( 
+		\PubCabin\Config	$_config, 
+		\PubCabin\Cabin		$_cabin 
+	) {
+		$this->config	= $_config;
+		$this->cabin	= $_cabin;
+	}
+	
+	/**
+	 *  Get or guess current server protocol
+	 *  
+	 *  @param string	$assume		Default protocol to assume if not given
+	 *  @return string
+	 */
+	public function getProtocol( string $assume = 'HTTP/1.1' ) : string {
+		if ( isset( $this->protocol ) ) {
+			return $this->protocol;
+		}
+		$this->protocol = $_SERVER['SERVER_PROTOCOL'] ?? $assume;
+		return $this->protocol;
+	}
+}
+
+
