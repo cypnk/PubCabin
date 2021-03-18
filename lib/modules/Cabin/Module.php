57869c2d0f81375b16c14e218cc6aafaa3d4dc9c
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2021-03-18 05:01:16 -0400

    Update Module.php

--- lib/modules/Cabin/Module.php
+++ lib/modules/Cabin/Module.php
@@ -29,7 +29,7 @@ class Module extends \PubCabin\Modules\Module {
 		}
 		
 		$this->request	= 
-		new \PubCabin\Request( static::config );
+		new \PubCabin\Request( static::$config );
 	}
 	
 	public function getRequest() {
