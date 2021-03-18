3abb53f72c8511c463752368193315aa22b15f4d
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2021-03-18 04:42:05 -0400

    Create Module.php

---
+++ lib/modules/Hooks/Module.php
@@ -0,0 +1,14 @@
+<?php declare( strict_types = 1 );
+/**
+ *  @file	/libs/modules/Hooks/Module.php
+ *  @brief	Delayed or scheduled execution routines
+ */
+namespace PubCabin\Modules\Hooks;
+
+class Module extends \PubCabin\Modules\Module {
+	
+	public function dependencies() : array {
+		return [];
+	}
+	
+}
