aa4e9d5cf43d4c171995fcb9f9c7c8f8576377dd
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2021-03-18 11:39:58 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 # Test server installation helper
+# Set the web user and group (used by PHP to read/write to these)
+# On Arch linux:
+W_USER=http
+
+# On OpenBSD:
+#W_USER=www
 
 # On Arch linux:
 SVR=/usr/share/nginx/pubcabin
@@ -15,6 +21,9 @@ DATE=`date +%Y-%m-%d-%H-%M-%S`
 mkdir -p $SVR/../snaps-$DATE
 mv -b $SVR/* $SVR/../snaps-$DATE
 cp -r -n * $SVR/
+
+chown -R $W_USER $SVR/data
+chmod -R 0755 $SVR/data
 
 cd $SVR/data
 
