b9fb01d7b3f3dc23504c596e776aede859a477c6
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2021-03-18 20:06:23 -0400

    Create Site.php

---
+++ lib/src/Core/Site.php
@@ -0,0 +1,45 @@
+<?php declare( strict_types = 1 );
+/**
+ *  @file	/lib/src/Core/Site.php
+ *  @brief	Website base data object
+ */
+
+namespace PubCabin\Core;
+
+class Site extends \PubCabin\Entity {
+	
+	/**
+	 *  Site description
+	 *  @var string
+	 */ 
+	public $label;
+	
+	/**
+	 *  Domain or IP address
+	 *  @var string
+	 */
+	public $basename;
+	
+	/**
+	 *  URL or sub section path
+	 *  @var string
+	 */
+	public $basepath;
+	
+	/**
+	 *  Site is currently active if true
+	 *  @var bool
+	 */
+	public $is_active;
+	
+	/**
+	 *  Site under maintenance if true
+	 *  @var bool
+	 */
+	public $is_maintenance;
+	
+	// TODO
+	public function save() { }
+	
+}
+
