7b8a9bf81cf2b5b629d0a1e1b4665edbad96678a
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2021-12-09 11:43:31 -0500

    Create Module.php

---
+++ lib/modules/Manager/Module.php
@@ -0,0 +1,13 @@
+<?php declare( strict_types = 1 );
+/**
+ *  @file	/lib/modules/Manager/Module.php
+ *  @brief	Backend major sections and user handling
+ */
+namespace PubCabin\Modules\Manager;
+
+class Module extends \PubCabin\Modules\Module {
+	
+	public function dependencies() : array {
+		return [ 'Styles', 'Forms', 'Menues', 'Sites', 'Membership' ];
+	}
+}
