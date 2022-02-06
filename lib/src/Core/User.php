dbb276d0423721b49ff65c4917e37151aa5bf5a7
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-02-06 01:58:10 -0500

    Update User.php

--- lib/src/Core/User.php
+++ lib/src/Core/User.php
@@ -94,6 +94,12 @@ class User extends \PubCabin\Entity {
 	 */
 	protected $_roles	= [];
 	
+	/**
+	 *  Currently active public keys
+	 *  @var array
+	 */
+	protected $_pub_keys	= [];
+	
 	
 	
 	
@@ -169,6 +175,7 @@ class User extends \PubCabin\Entity {
 	 */
 	protected $_auth_expires;
 	
+	
 	// TODO
 	public function save( \PubCabin\Data $data ) : bool {
 		if ( isset( $this->id ) ) {
@@ -209,6 +216,12 @@ class User extends \PubCabin\Entity {
 					$this->_roles = $value;
 				}
 				break;
+				
+			case 'public_keys':
+				if ( \is_array( $value ) ) {
+					$this->_pub_keys = $value;
+				}
+				break;
 			
 			case 'is_approved':
 				$this->_is_approved = ( bool ) $value;
@@ -242,6 +255,9 @@ class User extends \PubCabin\Entity {
 			
 			case 'roles':
 				return $this->_roles;
+			
+			case 'public_keys':
+				return $this->_pub_keys;
 			
 			case 'is_approved':
 				return $this->_is_approved ?? false;
@@ -415,8 +431,9 @@ class User extends \PubCabin\Entity {
 	 *  Authentication creation helper
 	 * 
 	 *  @param \PubCabin\Data	$data	Storage handler
+	 *  @return bool
 	 */
-	private function createAuth( \PubCabin\Data $data ) {
+	private function createAuth( \PubCabin\Data $data ) : bool {
 		$params = [
 			':user_id'	=> $this->id,
 			':email'	=> $this->email ?? '',
@@ -443,8 +460,9 @@ class User extends \PubCabin\Entity {
 			// Third party logins may have an expiration
 			$params[':expires'] = $this->auth_expires;
 		}
-
-		$data->setInsert( $sql, $params, static::MAIN_DATA );	
+		$id = $data->setInsert( $sql, $params, static::MAIN_DATA );
+		
+		return empty( $id ) ? false : true;
 	}
 	
 	/**
@@ -482,7 +500,7 @@ class User extends \PubCabin\Entity {
 	}
 	
 	/**
-	 *  Reset public/secret keypairs and return generated secret key
+	 *  Create public/secret keypair and return generated secret key
 	 *  
 	 *  @param \PubCabin\Data	$data		Storage handler
 	 *  @param int			$id		Logged in user's ID
@@ -490,7 +508,7 @@ class User extends \PubCabin\Entity {
 	 *  @param string		$expires	Key expiration date
 	 *  @return string
 	 */
-	public static function resetKeypair( 
+	public static function createKeypair( 
 		\PubCabin\Data	$data, 
 		int		$id, 
 		?string		$label,
@@ -501,26 +519,52 @@ class User extends \PubCabin\Entity {
 			return '';
 		}
 		
-		$db	= $data->getDb( static::MAIN_DATA );
-		$stm	= 
-		$db->prepare( 
-			"UPDATE public_keys SET public_key = :pubkey, 
-				label = :label, expires = :expires
-				WHERE user_id = :id;" 
-		);
+		$sql	= 
+		"INSERT INTO public_keys( user_id, public_key, label, expires ) 
+			VALUES( :user_id, :public_key, :label, :expires );";
 		$params = [
-			':pubkey'	=> $keys['public'],
+			':user_id'	=> $id,
+			':public_key'	=> $keys['public'],
 			':label'	=> empty( $label ) ? 
 				null : \PubCabin\Util::title( $label ),
 			':expires'	=> empty( $expires ) ? 
-				null : \PubCabin\Util::utc( $expires ),
-			':id'		=> $id
+				null : \PubCabin\Util::utc( $expires )
 		];
 		
-		if ( $stm->execute( $params ) ) {
-			return $keys['secret'];
+		return
+		$data->setInsert( $sql, $params, static::MAIN_DATA ) ? 
+			$keys['secret'] : '';
+	}
+	
+	/**
+	 *  Return given user's active (unexpired) public keys
+	 * 
+	 *  @param \PubCabin\Data	$data		Storage handler
+	 *  @param int			$id		Designated user ID
+	 */
+	public static function getPubKeys( 
+		\PubCabin\Data	$data, 
+		int		$id 
+	) : array {
+		$sql		= 
+		"SELECT public_key FROM public_keys WHERE id = :id 
+			AND (
+				strftime( '%s', expires ) > 
+				strftime( '%s', 'now' ) 
+			);";
+		$results	= 
+		$data->getResults( 
+			$sql, [ ':id' => $id ], static::MAIN_DATA
+		);
+		
+		if ( empty( $results ) ) {
+			return [];
 		}
-		return '';
+		$keys		= [];
+		foreach ( $results as $res ) {
+			$keys[] = $res['public_key'] ?? '';
+		}
+		return $keys;
 	}
 	
 	/**
