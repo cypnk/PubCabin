b229d2ae52a3b2722cb6c26304305eb855396e04
Rustic Cyberpunk <32854305+cypnk@users.noreply.github.com>
2022-08-18 15:19:32 -0400

    Create main.db.install.0.sql

---
+++ lib/modules/Blog/install/main.db.install.0.sql
@@ -0,0 +1,38 @@
+
+INSERT INTO page_types (
+	label, render, behavior
+) VALUES ( 'blog', '', '{
+	"parents":	["*", "page"],
+	"children":	["post"],
+ 	"label:		"{lang:ptypes:blog}",
+ 	"description":	"{lang:ptypes:blogdesc}",
+ 	"allow_children": 0,
+ 	"allow_comments": 0,
+ 	"privileges"	: {
+		"create_roles"	: [],
+		"edit_roles"	: [],
+		"delete_roles"	: [],
+		"upload_roles"	: [],
+		"mod_roles"	: [],
+		"comment_roles"	: []
+	},
+	"settings"	: {}
+}' ),
+( 'post', '', '{
+	"parents":	["blog"],
+	"children":	[],
+ 	"label":	"{lang:ptypes:post}",
+ 	"description":	"{lang:ptypes:postdesc}",
+ 	"allow_children": 1,
+ 	"allow_comments": 1,
+ 	"privileges"	: {
+		"create_roles"	: [],
+		"edit_roles"	: [],
+		"delete_roles"	: [],
+		"upload_roles"	: [],
+		"mod_roles"	: [],
+		"comment_roles"	: []
+	},
+	"settings"	: {}
+}' );
+
